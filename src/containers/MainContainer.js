7e4551810dd4d7a8e7578e2e8e5f6b86b72b8746
rasdinho <ras.dinho7@gmail.com>
2020-05-20 02:52:40 -0400

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,76 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  constructor(){
+    super()
+    this.state ={
+      allStocks: [],
+      stocks: [],  // same here u update it an pass it
+      portfolio: [], //after you update it u need to pass it as this.state.portfolio
+      price: false,
+      alpha: false,
+      type: null
+    }
+  }
+
+
+componentDidMount(){
+  fetch("http://localhost:3000/stocks")
+  .then(resp => resp.json())
+  .then(stocksArr => 
+    this.setState({
+      stocks: stocksArr, allStocks: stocksArr
+    })
+    )
+}
+
+handleAddStock =(stock, e) =>{
+  console.log("this adds buddy")
+  this.setState({
+    portfolio: [...this.state.portfolio, stock]
+  })
+}
+
+removeStock = (stock) =>{
+  console.log("this removes buddy")
+  let portfolioStocks = this.state.portfolio
+  portfolioStocks = portfolioStocks.filter(currentStock => currentStock.id !== stock.id)
+  this.setState({portfolio: portfolioStocks})
+  console.log(portfolioStocks)
+}
+
+handleFilter = (event) => {
+  console.log("clicked")
+
+  let filterName = event.target.value
+  if(filterName === "Alphabetically"){
+    this.setState({price: false, alpha: true, stocks: this.state.stocks.sort((a, b) => a.name.localeCompare(b.name))})
+  }else if(filterName === "Price"){
+    this.setState({price: true, alpha: false, stocks: this.state.stocks.sort(function(obj1, obj2){
+      // Ascending: first age less than the previous
+      return obj1.price - obj2.price;
+    })})
+  }else{
+    this.setState({stocks: this.state.allStocks.filter(stock => stock.type === event.target.value)})
+  }
+
+ 
+
+}
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar price={this.state.price} alpha={this.state.alpha} handleFilter={this.handleFilter} />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.stocks} action={this.handleAddStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.portfolio} action={this.removeStock}/>
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
-
+  
   render() {
+    console.log(this.props.portfolio)
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            //render your portfolio stocks here 
+            this.props.portfolio.map(stock => <Stock stock={stock} key={stock.id} action={this.props.action}/>)
+
           }
       </div>
     );
